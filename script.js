abf674039648a5ecc7cf38de835feae1e774cdd7
Szymsi <sienkiewicz.szymon@gmail.com>
2016-11-27 18:52:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 var text = "Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach";
 var elephant = "Zielone Słonie";
 var elephantUpperCased = elephant.toUpperCase();
-console.log(elephantUpperCased);
-var textAfter = text.replace("Papugi", elephant.toUpperCase());
-console.log(textAfter);
-console.log(textAfter.slice(0, 45));
+var textAfterSplit = text.split(" ");
+var halfOfSentence= finalSentence.length/2;
+var endOfTask = finalSentence.slice(0,halfOfSentence);
+var finalSentence = textAfterSplit.join(" ");
+textAfterSplit.splice(0, 1, elephantUpperCased);
+console.log(endOfTask);
